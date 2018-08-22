87802fe7341eeb1e00c3af2a39cd979dd24ecfb6
Andreas Schreiner <schreiner.andreas@outlook.com>
2018-08-22 22:58:27 +0200

    1.0.0.0

---
+++ de.fearvel.io/DirectoryTools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace de.fearvel.io
+{
+    static class DirectoryTools
+    {
+        public static void CreateHiddenDirectory(string path)
+        {
+            if (!System.IO.Directory.Exists(path))
+            {
+                DirectoryInfo di = System.IO.Directory.CreateDirectory(path);
+                di.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+            }
+        }
+    }
+}
---
+++ tester/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using de.fearvel.io;
+namespace tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "testdir";
+            var a = new FnPakManager();
+            //a.OpenFnPak(@"C:\Users\schreiner.andreas\Code\GitHub\TestPlugin.zip", "plugins");
+            //FileTools.EncryptFile(@"C:\Users\schreiner.andreas\Code\GitHub\TestPlugin.zip", @"C:\Users\schreiner.andreas\Code\GitHub\TestPlugin.fnPAK", "adadwqeq");
+            a.OpenFnPak(@"C:\Users\schreiner.andreas\Code\GitHub\TestPlugin.fnPAK", "TEST", "adadwqeq");
+        }
+    }
+}
